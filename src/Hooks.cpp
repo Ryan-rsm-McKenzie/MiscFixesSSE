553a45b2e1a13d8024970d6b9a08fd57d36489ef
SniffleMan <ryan__mckenzie@hotmail.com>
2018-12-05 20:02:48 -0800

    1.5.62 offsets

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -23,7 +23,10 @@ namespace Hooks
 
 		static void InstallHooks()
 		{
-			RelocAddr<uintptr_t> call_IsRunning(0x002DB800 + 0x22);
+			// 48 89 5C 24 08 57 48 83  EC 20 33 FF 49 8B D9 49  89 39 48 85 C9 ?? ?? 80  79 1A 3E 48 0F 44 F9 48  8B CF ?? ?? ?? ?? ?? 84
+			constexpr uintptr_t GameFunc_Native_IsRunning = 0x002DB800;	// 1_5_62
+
+			RelocAddr<uintptr_t> call_IsRunning(GameFunc_Native_IsRunning + 0x22);
 			g_branchTrampoline.Write5Call(call_IsRunning.GetUIntPtr(), GetFnAddr(&Hook_IsRunning));
 			_DMESSAGE("[DEBUG] Installed hooks for (%s)", typeid(ActorEx).name());
 		}
@@ -58,7 +61,8 @@ namespace Hooks
 
 		static void InstallHooks()
 		{
-			constexpr uintptr_t TES_OBJECT_BOOK_VTBL = 0x01573318;
+			constexpr uintptr_t TES_OBJECT_BOOK_VTBL = 0x01573318;	// 1_5_62
+
 			RelocPtr<_LoadBuffer_t*> vtbl_LoadBuffer(TES_OBJECT_BOOK_VTBL + (0x0F * 0x8));
 			orig_LoadBuffer = *vtbl_LoadBuffer;
 			SafeWrite64(vtbl_LoadBuffer.GetUIntPtr(), GetFnAddr(&Hook_LoadBuffer));
@@ -72,7 +76,10 @@ namespace Hooks
 
 	void PatchUseAfterFree()
 	{
-		RelocAddr<uintptr_t> BadUse(0x133C590 + 0x1AFD);
+		// 48 8B C4 48 89 50 10 55  53 56 57 41 54 41 55 41  56 41 57 48 8D A8 38 FB
+		constexpr uintptr_t BadUseFuncBase = 0x0133C590;	// 1_5_62
+
+		RelocAddr<uintptr_t> BadUse(BadUseFuncBase + 0x1AFD);
 		UInt8 patch[] = { 0x4D, 0x8B, 0xCF, 0x90, 0x90, 0x90, 0x90 };
 		// mov r9, 15
 		// nop
